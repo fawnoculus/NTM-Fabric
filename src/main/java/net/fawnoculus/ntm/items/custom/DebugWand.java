5867bc301dee4aa9fec37e3ba8ce49186a965763
tmat <torbenmattis90@gmail.com>
2025-06-15 19:27:08 +0200

    Ups

---
+++ src/main/java/net/fawnoculus/ntm/items/custom/DebugWand.java
@@ -0,0 +1,47 @@
+package net.fawnoculus.ntm.items.custom;
+
+import net.minecraft.component.type.TooltipDisplayComponent;
+import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.entity.damage.DamageType;
+import net.minecraft.entity.damage.DamageTypes;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.tooltip.TooltipType;
+import net.minecraft.registry.entry.RegistryEntry;
+import net.minecraft.text.Text;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.util.math.Vec3d;
+import net.minecraft.world.World;
+import net.minecraft.world.explosion.AdvancedExplosionBehavior;
+import net.minecraft.world.explosion.ExplosionBehavior;
+
+import java.util.Optional;
+import java.util.function.Consumer;
+
+public class DebugWand extends Item {
+  public DebugWand(Settings settings) {
+    super(settings);
+  }
+  
+  @Override
+  public ActionResult use(World world, PlayerEntity user, Hand hand) {
+    if(world.isClient()){
+      return ActionResult.SUCCESS;
+    }
+    
+    Vec3d targetPos = user.raycast(256, 0, false).getPos();
+    if(targetPos != null) {
+      ExplosionBehavior explosionBehavior = new AdvancedExplosionBehavior(true, true, Optional.empty(), Optional.empty());
+      world.createExplosion(user,  null, explosionBehavior, targetPos, 20, true, World.ExplosionSourceType.MOB);
+    }
+    
+    return ActionResult.SUCCESS_SERVER;
+  }
+  
+  @Override
+  public void appendTooltip(ItemStack stack, TooltipContext context, TooltipDisplayComponent displayComponent, Consumer<Text> tooltip, TooltipType type) {
+    tooltip.accept(Text.translatable("tooltip.ntm.debug_wand"));
+  }
+}
