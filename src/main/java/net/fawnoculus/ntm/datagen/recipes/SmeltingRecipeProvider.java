61a04fbf3761caafe1eae54fdf145c81cf619926
tmat <torbenmattis90@gmail.com>
2025-06-16 22:42:51 +0200

    Some ore smelting

--- src/main/java/net/fawnoculus/ntm/datagen/recipes/SmeltingRecipeProvider.java
+++ src/main/java/net/fawnoculus/ntm/datagen/recipes/SmeltingRecipeProvider.java
@@ -27,24 +27,35 @@ public class SmeltingRecipeProvider extends FabricRecipeProvider {
     return new RecipeGenerator(registryLookup, exporter) {
       @Override
       public void generate() {
-        offerSmelting(
-            URANIUM_ORES,
-            RecipeCategory.MISC,
-            ModItems.URANIUM_INGOT,
-            1.0f,
-            300,
-            "uranium"
-        );
-        offerBlasting(
-            URANIUM_ORES,
-            RecipeCategory.MISC,
-            ModItems.URANIUM_INGOT,
-            0.5f,
-            500,
-            "uranium"
-        );
+        float xp = 1.0f;
+        int time = 300;
+        makeOreSmelts(this, xp, time, "uranium", ModItems.URANIUM_INGOT, List.of(ModItems.RAW_URANIUM, ModBlocks.URANIUM_ORE, ModBlocks.NETHER_URANIUM_ORE, ModBlocks.SCHIST_URANIUM_ORE));
+        makeOreSmelts(this, xp, time, "scorched_uranium", ModItems.URANIUM_INGOT, List.of(ModItems.RAW_SCORCHED_URANIUM, ModBlocks.SCORCHED_URANIUM_ORE, ModBlocks.SCORCHED_NETHER_URANIUM_ORE, ModBlocks.SCORCHED_SCHIST_URANIUM_ORE));
+        makeOreSmelts(this, xp, time, "australium", ModItems.AUSTRALIUM_INGOT, List.of(ModItems.RAW_AUSTRALIUM, ModBlocks.AUSTRALIUM_ORE));
+        makeOreSmelts(this, xp, time, "lead", ModItems.LEAD_INGOT, List.of(ModItems.RAW_LEAD, ModBlocks.LEAD_ORE));
+        makeOreSmelts(this, xp, time, "plutonium", ModItems.PLUTONIUM_INGOT, List.of(ModItems.RAW_PLUTONIUM, ModBlocks.NETHER_PLUTONIUM_ORE));
+        makeOreSmelts(this, xp, time, "thorium", ModItems.THORIUM_232_INGOT, List.of(ModItems.RAW_THORIUM, ModBlocks.THORIUM_ORE));
       }
     };
+  }
+  
+  private void makeOreSmelts(RecipeGenerator generator, float xp, int time, String group ,ItemConvertible result, List<ItemConvertible> ores){
+    generator.offerSmelting(
+        ores,
+        RecipeCategory.MISC,
+        result,
+        xp,
+        time,
+        group
+    );
+    generator.offerBlasting(
+        ores,
+        RecipeCategory.MISC,
+        result,
+        xp,
+        time/2,
+        group
+    );
   }
   
   @Override
