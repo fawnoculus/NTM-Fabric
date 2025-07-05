0054cc14076bd2c7692598e466d58873adb1c430
tmat <torbenmattis90@gmail.com>
2025-07-06 01:34:11 +0200

    I remembered why I added those

--- src/main/java/net/fawnoculus/ntm/items/custom/consumable/AwesomeItem.java
+++ src/main/java/net/fawnoculus/ntm/items/custom/consumable/AwesomeItem.java
@@ -1,5 +1,6 @@
 package net.fawnoculus.ntm.items.custom.consumable;
 
+import net.fawnoculus.ntm.entity.ModStatusEffects;
 import net.fawnoculus.ntm.items.ModItems;
 import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.effect.StatusEffects;
@@ -27,6 +28,7 @@ public class AwesomeItem extends Item {
     player.giveItemStack(new ItemStack(ModItems.EMPTY_SYRINGE));
     player.addStatusEffect(new StatusEffectInstance(StatusEffects.NAUSEA, 150, 4, false, false, true));
     player.addStatusEffect(new StatusEffectInstance(StatusEffects.SPEED, 700, 6, false, false, true));
+    player.addStatusEffect(new StatusEffectInstance(ModStatusEffects.RAD_X, 700, 9, false, false, true));
     player.addStatusEffect(new StatusEffectInstance(StatusEffects.HASTE, 700, 9, false, false, true));
     player.addStatusEffect(new StatusEffectInstance(StatusEffects.STRENGTH, 700, 24, false, false, true));
     player.addStatusEffect(new StatusEffectInstance(StatusEffects.HEALTH_BOOST, 700, 9, false, false, true));
