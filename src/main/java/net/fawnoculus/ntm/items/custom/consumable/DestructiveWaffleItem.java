a2d45a62554f79da37eb3dd80b391a31468663e0
tmat <torbenmattis90@gmail.com>
2025-07-07 23:23:50 +0200

    LET THEM KNOW

--- src/main/java/net/fawnoculus/ntm/items/custom/consumable/DestructiveWaffleItem.java
+++ src/main/java/net/fawnoculus/ntm/items/custom/consumable/DestructiveWaffleItem.java
@@ -2,8 +2,10 @@ package net.fawnoculus.ntm.items.custom.consumable;
 
 import net.fawnoculus.ntm.items.components.ModFoodComponents;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.text.Text;
 import net.minecraft.world.World;
 
 public class DestructiveWaffleItem extends Item {
@@ -13,6 +15,9 @@ public class DestructiveWaffleItem extends Item {
   
   @Override
   public ItemStack finishUsing(ItemStack stack, World world, LivingEntity user) {
+    if(user instanceof PlayerEntity player && !world.isClient()){
+      player.sendMessage(Text.literal("Now you would violently Explode, but Nuclear Explosions are not implemented yet"), false);
+    }
     // TODO: this, once we have Nuclear Explosions
     return super.finishUsing(stack, world, user);
   }
