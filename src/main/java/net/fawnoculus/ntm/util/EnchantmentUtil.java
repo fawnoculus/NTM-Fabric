1e265faedc6f1d4b79161d029ef7a156261fb603
tmat <torbenmattis90@gmail.com>
2025-07-16 16:44:31 +0200

    Attempt 1000 of fixing this thing

--- src/main/java/net/fawnoculus/ntm/util/EnchantmentUtil.java
+++ src/main/java/net/fawnoculus/ntm/util/EnchantmentUtil.java
@@ -7,36 +7,48 @@ import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.item.ItemStack;
 import net.minecraft.registry.*;
 import net.minecraft.registry.entry.RegistryEntry;
+import net.minecraft.world.World;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
+import java.util.Optional;
 import java.util.function.Predicate;
 
 public class EnchantmentUtil {
-  private static final Registry<Enchantment> registry = DynamicRegistryManager.of(Registries.REGISTRIES).getOrThrow(RegistryKeys.ENCHANTMENT);
+  //TODO: fix whatever is wrong with this
   
-  public static boolean hasEnchantment( ItemStack stack, RegistryKey<Enchantment> enchantmentRegistryKey){
-    return EnchantmentHelper.getLevel(getEnchantmentEntry(enchantmentRegistryKey), stack) > 0;
+  public static boolean hasEnchantment(@NotNull World world, RegistryKey<Enchantment> enchantmentKey, ItemStack stack){
+    return EnchantmentHelper.getLevel(getEnchantmentEntry(world, enchantmentKey), stack) > 0;
   }
   
-  public static void addEnchantment(@NotNull ItemStack stack, RegistryKey<Enchantment> enchantmentRegistryKey, int level){
-    RegistryEntry<Enchantment> enchantmentRegistryEntry = getEnchantmentEntry(enchantmentRegistryKey);
+  public static void addEnchantment(@NotNull World world, RegistryKey<Enchantment> enchantmentKey, int level ,@NotNull ItemStack stack){
+    RegistryEntry<Enchantment> enchantmentRegistryEntry = getEnchantmentEntry(world, enchantmentKey);
     stack.addEnchantment(enchantmentRegistryEntry, level);
   }
   
-  public static void removeEnchantment(@NotNull ItemStack stack, RegistryKey<Enchantment> enchantmentRegistryKey){
-    RegistryEntry<Enchantment> enchantment = getEnchantmentEntry(enchantmentRegistryKey);
-    
+  public static void removeEnchantment(@NotNull World world, RegistryKey<Enchantment> enchantmentKey, @NotNull ItemStack stack){
     ItemEnchantmentsComponent itemEnchantmentsComponent = stack.getOrDefault(DataComponentTypes.ENCHANTMENTS, ItemEnchantmentsComponent.DEFAULT);
     ItemEnchantmentsComponent.Builder builder = new ItemEnchantmentsComponent.Builder(itemEnchantmentsComponent);
-    builder.remove(Predicate.isEqual(enchantment));
+    builder.remove(Predicate.isEqual(getEnchantmentEntry(world, enchantmentKey)));
     
     stack.set(DataComponentTypes.ENCHANTMENTS, builder.build());
   }
   
-  public static @Nullable RegistryEntry<Enchantment> getEnchantmentEntry(@NotNull RegistryKey<Enchantment> enchantmentRegistryKey){
-    Enchantment enchantment = registry.get(enchantmentRegistryKey);
+  public static @NotNull RegistryEntry<Enchantment> getEnchantmentEntry(@NotNull World world, @NotNull RegistryKey<Enchantment> enchantmentKey){
+    Enchantment enchantment = world.getRegistryManager()
+        .getOrThrow(RegistryKeys.ENCHANTMENT)
+        .get(enchantmentKey);
+    if(enchantment == null){
+      Optional<RegistryEntry.Reference<Enchantment>> optional = world.getRegistryManager()
+              .getOrThrow(RegistryKeys.ENCHANTMENT)
+              .getDefaultEntry();
+      if(optional.isEmpty()){
+        throw new IllegalStateException("Enchantment Registry does not have a default Value to fall back to");
+      }
+      return optional.get();
+    }
     
-    return registry.getEntry(enchantment);
+    return world.getRegistryManager()
+        .getOrThrow(RegistryKeys.ENCHANTMENT)
+        .getEntry(enchantment);
   }
 }
